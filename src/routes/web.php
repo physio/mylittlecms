7c0cee56b9ea5b50253769dccb1ed5f75be9d398
physio <ilphysio@gmail.com>
2017-08-11 10:09:32 +0200

    aggiunte rotte per attivita

--- src/routes/web.php
+++ src/routes/web.php
@@ -9,6 +9,15 @@ Route::group([
 
         Route::post('contatti', ['as' => 'contatti_post', 'uses' => 'ContactController@store']);
 
+        
+
+        Route::get('/news/{slug}', 'ArticleController@index');
+
+        Route::get('/attivita/elenco/{title}', 'ServiceController@category');
+        Route::get('/attivita/{slug}', 'ServiceController@index');
+        
+        Route::get('/speciali/{slug}', 'SpecialController@index');
+
         Route::get('/notizie/elenco', 'ArticleController@index');
         Route::get('/notizie/dettaglio/{slug}', 'ArticleController@show');
 
