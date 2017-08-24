904136ee64aa61aa95624640050c8febda00969b
physio <ilphysio@gmail.com>
2017-08-24 06:57:33 +0000

    cambiata grid delle attivita

--- src/resources/views/service/grid.blade.php
+++ src/resources/views/service/grid.blade.php
@@ -1,41 +1,60 @@
 @extends('MyLittleCMS::layouts.default')
 
 @section('content')
-
-                                <h2>{{ $title }}</h2>
-
-                                    @foreach ($list as $activity) 
-                                    <div class="blog-item">
-                                        <div class="blog-thumbnail image-element">
-                                            <a href="/servizi/dettaglio/{{ $activity->slug }}" class="image-link black-white">
-                                                <img src="/{{ $activity->image }}" alt="" />
-                                                <div class="image-hover-icon bg-set">
-                                                    <i class="icon ion-link"></i>
-                                                </div>
-                                                <div class="image-hover-border"></div>
-                                            </a>
-                                        </div>
-                                        <div class="blog-info">
-                                            <div class="blog-title-excerpt">
-                                                <h2 class="blog-title">
-                                                    <a href="/servizi/dettaglio/{{ $activity->slug }}">{{ $activity->title }}</a>
-                                                </h2>
-                                                <div class="excerpt">
-                                                   {!! strip_tags(str_limit($activity->description, $limit = 250, $end = '...')) !!}
-                                                </div>
-                                            </div>
-                                            <div class="blog-meta-wrapper">
-                                                <ul class="blog-meta">
-                                                    <li class="readmore-link">
-                                                        <a href="/servizi/dettaglio/{{ $activity->slug }}">Leggi Tutto</a>
-                                                    </li>
-                                                </ul>
-                                            </div>
+                <!-- Start Banner Area-->
+                <div class="banner-area text-center">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="banner-content-wrapper">
+                                    <div class="banner-content">
+                                        <h2>I Nostri Corsi</h2>
+                                        <div class="banner-breadcrumb">
+                                            <ul>
+                                                <li><a href="index.html">home </a> <i class="zmdi zmdi-chevron-right"></i></li>
+                                                <li>{{ $title }}</li>
+                                            </ul>
                                         </div>
                                     </div>
-                                    @endforeach
-                                   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Banner Area-->
 
-            
+                <!--Start Category Area-->
+                <div class="category-area text-center pt-80 two">
+                    <div class="container">
+                        <div class="row">
+
+                            @foreach($list->chunk(3) as $items)
+                            <div class="col-md-4 col-sm-6 col-xs-12">
+                                @foreach($items as $item)
+                                     <div class="single-category mb-30">
+                                    <div class="category-img">
+                                        <img src="/{{ $item->image }}" alt="category">
+                                        <div class="category-hover">
+                                            <h3>{{ $item->title }}</h3>
+                                            <span></span>
+                                        </div>
+                                    </div>
+                                    <div class="category-content pt-20 pb-20">
+                                        <h6>{{ $item->fasciaEta }} </h6>
+                                        <p>{{ str_limit(strip_tags($item->description), $limit = 100, $end = '...') }} </p>
+                                        <div class="class-button">
+                                            <a href="/attivita/{{ $item->slug }}" class="read-more btn"><span>dettagli</span></a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                @endforeach
+                            </div>
+                            @endforeach
+
+                        </div>
+                    </div>
+                </div>
+                <!-- End of Category Area -->
 
 @endsection
